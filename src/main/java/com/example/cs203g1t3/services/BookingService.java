4acc538339ff3ef563c1da1a8ae4321c15290ab5
liewjunjie <junjie.liew.2022@scis.smu.edu.sg>
2023-10-28 13:38:28 +0800

    Merge and resolve conflicts between make booking implementation, get upcoming and past bookings implementation

--- src/main/java/com/example/cs203g1t3/services/BookingService.java
+++ src/main/java/com/example/cs203g1t3/services/BookingService.java
@@ -147,7 +147,7 @@
         // System.out.println(userService.getUser(bookingRequest.getUserId()));
 
         //make booking
-        Booking booking = new Booking(bookingStartTime,bookingEndTime,bookingRequest.getTimeBookingMade(),creditDeducted);
+        Booking booking = new Booking(bookingStartTime,bookingEndTime,LocalDateTime.now(),creditDeducted);
         booking.setFacility(facility);
         booking.setDateBooked(dateBooked);
         booking.setUser(user);
@@ -189,8 +189,6 @@
         }
         return result;
     }
-    // -------------------------- Change all implementations below this line
-    // ---------------------------------------
 
     public List<ViewPastBookingsResponse> getPastBookings(Long userId){
         List<Booking> upcomingBookings = bookingRepository.findBookingsByUserId(userId);
@@ -212,78 +210,7 @@
         }
         return result;
     }
-    // public Booking updateBooking(Long facilityId,Long bookingId, Booking
-    // newBooking) {
-    // //idea of this method
-    // //get old booking -> remove bookingRepository from current booking list
-    // //
-    //
-    // Booking booking = bookingRepository.findById(bookingId).orElse(null);
-    // //scuffed code
-    // if(booking == null){ //if booking not found
-    // throw new BookingNotFoundException(bookingId);
-    // }
-    //
-    // List<Booking> allBookings = getAllBookings();
-    // allBookings.remove(booking); //remove current booking from the list
-    //
-    // List<LocalTime> timingTaken = new ArrayList<>(); //list of all timings that
-    // is taken
-    //
-    // for(Booking tempBooking: allBookings){
-    // List<LocalTime> tempList = listBookingTimings(tempBooking);
-    // timingTaken.addAll(tempList);
-    // }
-    //
-    // List<LocalTime> newBookingTiming = listBookingTimings(newBooking);
-    //
-    // // !Collections.disjoint(list1, list2) returns true if there is overlap
-    // if(!Collections.disjoint(timingTaken, newBookingTiming)){
-    // return null;
-    // }
-    //
-    // Facility facility = facilities.findById(facilityId).orElse(null);
-    //
-    // List<LocalTime> facilityTimes = facility.getTimeSlots();
-    // facilityTimes.addAll(listBookingTimings(booking));
-    // facilityTimes.removeAll(newBookingTiming);
-    // Collections.sort(facilityTimes);
-    //
-    // facility.setTimeSlots(facilityTimes);
-    // facilities.save(facility);
-    //
-    // booking.setStartTime(newBooking.getStartTime());
-    // booking.setEndTime(newBooking.getEndTime());
-    // bookingRepository.save(booking);
-    // return booking;
-    //
-    // //old impl
-    // // return bookingRepository.findById(bookingId).map(booking -> {
-    // // booking.setStartTime(newBooking.getStartTime());
-    // // booking.setEndTime(newBooking.getEndTime());
-    // // return bookingRepository.save(booking);
-    // // }).orElse(null);
-    // }
-
-    // public boolean isValidBooking(LocalTime startTime, LocalTime endTime,
-    // Facility facility) {
-    // if(startTime.isAfter(endTime)){ //checking if startTime is after endTime
-    // return false;
-    // }
-    // List<LocalTime> allAvailableTimings = facility.getTimeSlots();
-    //
-    // //check if each 30 minute slot starting from start time is
-    // //available in the time available time slot in facility until the end time
-    // while(startTime.isBefore(endTime)){
-    // if(!allAvailableTimings.contains(startTime)){
-    // return false;
-    // }
-    // startTime = startTime.plusMinutes(30);
-    // }
-    // return true;
-    //
-    // }
 
 }
 
- }
+
