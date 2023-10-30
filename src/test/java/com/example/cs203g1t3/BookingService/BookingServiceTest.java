312c2e5221ebfd2479c8375f98877366b9382704
T-HQ <heqiang.tan.2022@scis.smu.edu.sg>
2023-10-30 17:33:59 +0800

    makeBookingTest

--- src/test/java/com/example/cs203g1t3/BookingService/BookingServiceTest.java
+++ src/test/java/com/example/cs203g1t3/BookingService/BookingServiceTest.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.time.*;
 import java.util.*;
 import com.example.cs203g1t3.repository.BookingRepository;
-import com.example.cs203g1t3.services.FacilityService;
-import com.example.cs203g1t3.services.TimeSlotService;
+import com.example.cs203g1t3.repository.FacilityDateRepository;
+import com.example.cs203g1t3.repository.FacilityRepository;
+import com.example.cs203g1t3.repository.TimeSlotsRepository;
 
 import jakarta.inject.Inject;
 
@@ -20,12 +21,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class BookingServiceTest {
     @Autowired
     private BookingService bookingService;
-    @Autowired
+    @InjectMocks
     private FacilityService facilityService;
+    @Mock
+    private TimeSlotsRepository timeSlotsRepository;
+    @Mock
+    private FacilityDateRepository facilityDateRepository;
+    @Mock 
+    private FacilityRepository facilityRepository;
 
+    
     @Test
     void make_booking_valid() throws Exception{
         List<LocalTime> timings = new ArrayList<>();
